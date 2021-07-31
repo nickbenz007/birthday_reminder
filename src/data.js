48c8868ebba8747a1021e0ae43815b88dd0fe20b
nick benz <nickbenz007@gmail.com>
2021-07-31 15:31:20 +0500

    initial version

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+    id: 1,
+    name: 'Barbara Aguilar',
+    age: 25,
+    image: '../images/1.jpg'
+},
+{
+    id: 2,
+        name: 'Juliana',
+    age: 20,
+    image: '../images/2.jpg'
+},
+{
+    id: 3,
+        name: 'Sandra',
+    age: 27,
+    image: '../images/3.jpg'
+},
+{
+    id: 4,
+    name: 'Marry',
+    age: 30,
+    image: '../images/4.jpg'
+},
+{
+    id: 5,
+    name: 'Debbie',
+    age: 29,
+    image: '../images/5.jpg'
+},
+];
